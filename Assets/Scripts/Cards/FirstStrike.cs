7e97646b8c35735fb3c56946396008a2b5ae7ff2
DZ-tt2195 <tt2195@nyu.edu>
2025-03-22 11:13:10 -0700

    add first strike, drop weakness

---
+++ Assets/Scripts/Cards/FirstStrike.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class FirstStrike : AuraCard
+{
+    protected override void Awake()
+    {
+        base.Awake();
+        this.bottomType = this.GetType();
+        this.coinCost = 5;
+        this.extraText = "Start of combat: Your Troop here does an attack.";
+        this.artistText = "Michael Watson\nDominion: Cornucopia & Guilds\n(Joust)";
+    }
+
+    public override void StartOfCombat(Entity entity, int logged)
+    {
+        MovingTroop troop = Manager.inst.FindMyTroop(entity.player, entity.currentRow);
+        if (troop != null)
+            troop.Attack(true, logged);
+    }
+}
